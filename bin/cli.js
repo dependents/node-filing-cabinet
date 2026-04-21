04a18bda85c92ddefc84d5b65e1ae46ab7868b86
XhmikosR <xhmikosr@gmail.com>
2026-04-21 22:32:49 +0300

    CLI: tweak help

--- bin/cli.js
+++ bin/cli.js
@@ -10,12 +10,12 @@ program
   .name(name)
   .description(description)
   .version(version)
-  .argument('<path>', 'the path to partial/dependency to examine')
-  .option('-d, --directory <path>', 'root of all files')
-  .option('-c, --config [path]', 'location of a RequireJS config file for AMD')
-  .option('-w, --webpack-config [path]', 'location of a Webpack config file')
-  .option('-t, --ts-config [path]', 'location of a TypeScript config file')
-  .option('-f, --filename [path]', 'file containing the dependency')
+  .argument('<path>', 'dependency path to resolve')
+  .option('-f, --filename <path>', 'file containing the dependency')
+  .option('-d, --directory <path>', 'project root used for resolution')
+  .option('-c, --config [path]', 'RequireJS config file (AMD resolution)')
+  .option('-w, --webpack-config [path]', 'Webpack config file')
+  .option('-t, --ts-config [path]', 'TypeScript config file')
   .showHelpAfterError()
   .parse();
 
